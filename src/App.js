9081478b337c6f6d675b20c375ab4bf9ba84126c
Darrion_White <wdarrion64@gmail.com>
2021-12-10 20:50:43 -0600

    could not get my code to work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [data1, setData1] = useState([])
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/1')
+      .then((res) => {
+        console.log(res);
+      })
+    }, []
+  )
+
+    return (
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        <Character data={data1} />
+      </div>
+    );
+  }
 
 export default App;
